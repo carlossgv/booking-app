106154b9605f2adb6b8d3f65c812aac1c510a09a
Carlos Gonzalez <carlossgv@gmail.com>
2021-11-25 22:12:51 -0300

    removed businesses resource

--- src/api/businesses/businesses.controller.ts
+++ src/api/businesses/businesses.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { BusinessesService } from './businesses.service';
-import { CreateBusinessDto } from './dto/create-business.dto';
-import { UpdateBusinessDto } from './dto/update-business.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { BusinessesService } from "./businesses.service";
+import { CreateBusinessDto } from "./dto/create-business.dto";
+import { UpdateBusinessDto } from "./dto/update-business.dto";
 
-@Controller('businesses')
+@Controller("businesses")
 export class BusinessesController {
   constructor(private readonly businessesService: BusinessesService) {}
 
@@ -17,18 +25,21 @@ export class BusinessesController {
     return this.businessesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.businessesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBusinessDto: UpdateBusinessDto) {
+  @Patch(":id")
+  update(
+    @Param("id") id: string,
+    @Body() updateBusinessDto: UpdateBusinessDto,
+  ) {
     return this.businessesService.update(+id, updateBusinessDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.businessesService.remove(+id);
   }
 }
